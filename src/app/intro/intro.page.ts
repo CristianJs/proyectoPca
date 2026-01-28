fcbc08f60cabd41677fb6bce5e7e935b79e3ada8
Cristian Atehortua <asistemas3@comfamiliar.com.co>
2026-01-28 15:52:22 -0500

    Crear intro utilizando variables css, utilities y más de ionic

--- src/app/intro/intro.page.ts
+++ src/app/intro/intro.page.ts
@@ -10,17 +10,44 @@ import { StorageService } from '../core/services/storage.service';
   templateUrl: './intro.page.html',
   styleUrls: ['./intro.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule],
+  imports: [IonContent,  CommonModule, IonToolbar, FormsModule],
   schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
 export class IntroPage{
+
+  slides : any = [
+    {
+      "id": 1,
+      "titulo": "Descubre algo nuevo",
+      "imagen": "assets/images/slide1.png",
+      "descripcion": "Descubre la musica que siempre quisiste escuchar solo con un click."
+    },
+    {
+      "id": 2,
+      "titulo": "Agrupa en un solo lugar",
+      "imagen": "assets/images/slide2.png",
+      "descripcion": "Crea playlists por genero para que cada día sepas que elegir."
+    },
+    {
+      "id": 3,
+      "titulo": "Comparte",
+      "imagen": "assets/images/slide3.png",
+      "descripcion": "Comparte la musica que escuchas y has que los buenos gustos perduren"
+    },
+    {
+      "id": 4,
+      "titulo": "Un nuevo mundo",
+      "imagen": "assets/images/slide4.png",
+      "descripcion": "Explora un nuevo mundo del cual no vas a querer salir, Bienvenido"
+    }
+  ]
 
   constructor(
     private readonly _router: Router,
     private readonly _storageService: StorageService
   ) { }
 
-  async goBack(){
+  async goHome(){
     await this._storageService.set('intro',"true");
     this._router.navigateByUrl('/home');
   }
